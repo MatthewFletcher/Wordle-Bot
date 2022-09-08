ce3dfb4556e5ca75e05bcce4410238b7a9b83e86
fletch <bassmattf@gmail.com>
2022-09-08 08:19:29 -0500

    initial commit

---
+++ wordle.py
@@ -0,0 +1,118 @@
+import string
+import os
+import re
+import sys
+from typing import Pattern
+
+
+import numpy as np
+from colorama import Back
+
+
+class Game:
+    def __init__(self, word_length: int = 5) -> None:
+        self.STAT_FILE = f"/tmp/stats{os.getpid()}.json"
+        self.WORD_FILE = "/Users/fletch-mac/Desktop/wordle_list.txt"
+        self.WORD_LEN = word_length
+        self.MAX_GUESS_NUM = 6
+        self.guessed_matrix = np.array(
+            [(np.zeros(5, dtype=int)) for _ in list(string.ascii_lowercase)],
+            dtype=object,
+        )
+        self.current_guess = ""
+        with open(self.WORD_FILE, "r") as f:
+            all_words = [word.strip() for word in f.readlines()]
+            self.VALID_WORDS = np.array(
+                [
+                    word
+                    for word in all_words
+                    if len(word) == self.WORD_LEN
+                    and all(c in string.ascii_letters for c in word)
+                ]
+            )
+            self.remaining_words = self.VALID_WORDS
+            self.word = np.random.choice(self.VALID_WORDS).lower()
+
+    def get_new_word(self) -> None:
+        # Reset word list
+        self.remaining_words = self.VALID_WORDS
+        # Reset word to guess
+        self.word = np.random.choice(self.VALID_WORDS).lower()
+
+    def get_guess(self, prompt="Enter word: ") -> None:
+        while True:
+            try:
+                w = input(prompt).lower()
+            except EOFError as e:
+                if input("Are you sure you want to end?"):
+                    sys.exit()
+            if w in self.VALID_WORDS:
+                break
+            else:
+                print(f"{w} is not a valid guess")
+        self.current_guess = w
+
+    def generate_regex(self) -> re.Pattern:
+        re_str = ['.' for _ in range(self.WORD_LEN)]
+        for idx,pos_arr in enumerate(self.guessed_matrix.T):
+            #If any letters have score=2, set the corresponding
+            # regex value to that letter
+            if any(pos_arr==2):
+                re_str[idx]=chr( ord('a') + pos_arr.argmax())
+            if any(pos_arr==-1):
+                #re_str[idx]
+                pass
+        return re.compile("".join(re_str))
+
+    def get_possible_words(self) -> list:
+        r = self.generate_regex()
+        # Create list of words that are still valid options
+        ret_arr = [word for word in self.VALID_WORDS if r.match(word)]
+        return ret_arr
+
+    def check_guess(self) -> np.ndarray:
+        ret_arr = np.zeros(self.WORD_LEN, dtype=int)
+        for idx, guess_letter, word_letter in zip(
+            range(self.WORD_LEN), self.current_guess, self.word
+        ):
+            if guess_letter == word_letter:
+                self.guessed_matrix[ord(guess_letter) - ord("a")][idx] = 2
+                ret_arr[idx] = 2
+            elif guess_letter in str(self.word):
+                self.guessed_matrix[ord(guess_letter) - ord("a")][idx] = 1
+                ret_arr[idx] = 1
+            else:
+                self.guessed_matrix[ord(guess_letter) - ord("a")][idx] = -1
+                ret_arr[idx] = 0
+
+        return ret_arr
+
+    def print_guess(self) -> None:
+        print_str = ""
+        guess_arr = self.check_guess()
+        for guess_letter, word_letter in zip(self.current_guess, self.word):
+            if guess_letter == word_letter:
+                print_str += Back.GREEN + guess_letter
+            elif guess_letter in str(self.word):
+                print_str += Back.YELLOW + guess_letter
+            else:
+                print_str += Back.LIGHTBLACK_EX + guess_letter
+        print_str += Back.RESET
+        print(print_str)
+
+    def play_person_game(self):
+        print(f"Word value: {self.word}")
+
+        for i in range(self.MAX_GUESS_NUM):
+            print(f"On guess {i}")
+            self.get_guess("Enter guess here: ")
+            guess_arr = self.check_guess()
+            self.print_guess()
+            if sum(guess_arr) == 2*self.WORD_LEN:
+                print("Success")
+                break
+        print("Game Over")
+
+
+g = Game()
+g.play_person_game()
